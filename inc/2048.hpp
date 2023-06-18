82d8f4ee9221123e5b6a0266222e909002b28421
Daniel <boggless@pm.me>
2023-06-17 19:45:15 -0700

    I worked ahead to see if I knew how to do it.

--- inc/2048.hpp
+++ inc/2048.hpp
@@ -1,11 +1,14 @@
+#define ROWS    4
+#define COLS    4
+#define TILES   ROWS*COLS
+#define INDEX(r,c) r*COLS+c
 class Game2048{
     public:
-    class Tile{
+    struct Tile{
         int value; // 0 means empty
-        int pos_x, pos_y;
     };
 
-    Tile board[16];
+    Tile board[TILES];
 
     int score;
 
@@ -16,7 +19,13 @@ class Game2048{
     void move_up();
     void move_down();
 
+    bool move(char dir);
+
     int get_score();
 
     void spawn_tile();
+
+    Game2048();
+
+    void display_console();
 };
--- src/2048.cpp
+++ src/2048.cpp
@@ -1,5 +1,185 @@
+#include <iostream>
+#include <random>
 #include <2048.hpp>
 
+void Game2048::restart(){
+    // set score to 0
+    score = 0;
+    // make all tiles blank
+    for(int i = 0; i < 16; i++){
+        board[i].value = 0;
+    }
+
+    // spawn 4 tiles;
+    for(int i = 0; i < 4; i++){
+        spawn_tile();
+    }
+}
+
+void Game2048::move_left(){
+    for(int r = 0; r < ROWS; r++){
+        int leftmost = 0;
+        for(int c = 0; c < COLS; c++){
+            int value = board[INDEX(r,c)].value;
+            if(value == 0) continue;
+
+            board[INDEX(r,c)].value = 0;
+            if(board[INDEX(r,leftmost)].value == 0){
+                board[INDEX(r,leftmost)].value = value;
+            }
+            else if(board[INDEX(r,leftmost)].value == value){
+                board[INDEX(r,leftmost)].value = 2 * value;
+                leftmost++; // prevent double combinations
+            } else{
+                leftmost++;
+                board[INDEX(r,leftmost)].value = value;
+            }
+        }
+    }
+
+    spawn_tile();
+}
+void Game2048::move_right(){
+    for(int r = 0; r < ROWS; r++){
+        int rightmost = COLS-1;
+        for(int c = COLS-1; c >= 0; c--){
+            int value = board[INDEX(r,c)].value;
+            if(value == 0) continue;
+            
+            board[INDEX(r,c)].value = 0;
+            if(board[INDEX(r,rightmost)].value == 0){
+                board[INDEX(r,rightmost)].value = value;
+            }
+            else if(board[INDEX(r,rightmost)].value == value){
+                board[INDEX(r,rightmost)].value = 2 * value;
+                rightmost--; // prevent double combinations
+            } else{
+                rightmost--;
+                board[INDEX(r,rightmost)].value = value;
+            }
+        }
+    }
+
+    spawn_tile();
+}
+void Game2048::move_up(){
+    for(int c = 0; c < COLS; c++){
+        int topmost = 0;
+        for(int r = 0; r < ROWS; r++){
+            int value = board[INDEX(r,c)].value;
+            if(value == 0) continue;
+
+            board[INDEX(r,c)].value = 0;
+            if(board[INDEX(topmost,c)].value == 0){
+                board[INDEX(topmost,c)].value = value;
+            }
+            else if(board[INDEX(topmost,c)].value == value){
+                board[INDEX(topmost,c)].value = 2 * value;
+                topmost++; // prevent double combinations
+            } else{
+                topmost++;
+                board[INDEX(topmost,c)].value = value;
+            }
+        }
+    }
+
+    spawn_tile();
+}
+void Game2048::move_down(){
+    for(int c = 0; c < COLS; c++){
+        int topmost = ROWS-1;
+        for(int r = ROWS-1; r >= 0; r--){
+            int value = board[INDEX(r,c)].value;
+            if(value == 0) continue;
+
+            board[INDEX(r,c)].value = 0;
+            if(board[INDEX(topmost,c)].value == 0){
+                board[INDEX(topmost,c)].value = value;
+            }
+            else if(board[INDEX(topmost,c)].value == value){
+                board[INDEX(topmost,c)].value = 2 * value;
+                topmost--; // prevent double combinations
+            } else{
+                topmost--;
+                board[INDEX(topmost,c)].value = value;
+            }
+        }
+    }
+
+    spawn_tile();
+}
+
+bool Game2048::move(char dir){
+    if(dir == 'q') return false;
+    if(dir == '1' || dir == 'A' || dir == 'a'){
+        move_left();
+    }
+    if(dir == '2' || dir == 'D' || dir == 'd'){
+        move_right();
+    }
+    if(dir == '3' || dir == 'W' || dir == 'w'){
+        move_up();
+    }
+    if(dir == '4' || dir == 'S' || dir == 's'){
+        move_down();
+    }
+    // return game_end;
+    return true;
+}
+
+int Game2048::get_score(){
+    return score;
+}
+
+void Game2048::spawn_tile(){
+    int value = rand() % 4;
+    if(value == 0) value = 4;
+    else value = 2; 
+
+    int available = 0;
+    for(int i = 0; i < TILES; i++){
+        if(board[i].value == 0) available++; 
+    }
+
+    if(available == 0){
+        // end game
+        std::cerr << "Game should end!" << std::endl;
+    }
+
+    int random_spot = rand() % available;
+    for(int i = 0; i < TILES; i++){
+        if(board[i].value == 0) {
+            if(random_spot-- == 0){
+                board[i].value = value;
+            }
+        }
+    }
+    
+}
+
+Game2048::Game2048(){
+    restart();
+}
+
+void Game2048::display_console(){
+    std::cout << "2048!!!\t\tScore: " << score << std::endl;
+    for(int i = 0; i < ROWS; i++){
+        std::cout << "\n|\t";
+        for(int j = 0; j < COLS; j++){
+            std::cout << board[i * COLS + j].value << "|\t";
+        }
+    }
+    std::cout << std::endl << std::endl;
+}
+
 int main(){
-    return 0;
+    srand(time(nullptr));
+    Game2048 game = Game2048();
+    while(true){
+        system("cls");
+        game.display_console();
+        char input;
+        std::cin >> input;
+        if(!game.move(input)) break;
+    }
 }
